5d31d693480ec251c167a1db11e3a392e38aab65
Alwin Esch <alwin.esch@web.de>
2021-09-27 22:07:11 +0200

    "libdsdpcm": remove values from DSDPCMConverter
    This values on class produce Windows compile errors:
    ```
    	real_t  dsd_fir1_8_ctables[CTABLES(DSDFIR1_8_LENGTH)][256];
    	real_t  dsd_fir1_16_ctables[CTABLES(DSDFIR1_16_LENGTH)][256];
    	real_t  dsd_fir1_64_ctables[CTABLES(DSDFIR1_64_LENGTH)][256];
    	real_t  pcm_fir2_2_coefs[PCMFIR2_2_LENGTH];
    	real_t  pcm_fir3_2_coefs[PCMFIR3_2_LENGTH];
    ```
    
    As it works also without are they removed, but not sure if that affect something!
    On test it works without, but there must be reason why there?
    
    This was the Windows error:
    ```
    DSDPCMConverterEngine.cpp
    d:\jenkins\workspace\binary-addons\kodi-windows-x86_64-matrix\tools\depends\target\binary-addons\audiodecoder.sacd\lib\libdsdpcm\DSDPCMConverter.h(42): error C3779: '<lambda_389370868e5d87e756fda3a02ce50c72>::operator ()': a function that returns 'auto' cannot be used before it is defined
    d:\jenkins\workspace\binary-addons\kodi-windows-x86_64-matrix\tools\depends\target\binary-addons\audiodecoder.sacd\lib\libdsdpcm\DSDPCMConstants.h(28): note: see declaration of '<lambda_389370868e5d87e756fda3a02ce50c72>::operator ()'
    d:\jenkins\workspace\binary-addons\kodi-windows-x86_64-matrix\tools\depends\target\binary-addons\audiodecoder.sacd\lib\libdsdpcm\DSDPCMConverter.h(84): note: see reference to class template instantiation 'DSDPCMConverter<real_t>' being compiled
    d:\jenkins\workspace\binary-addons\kodi-windows-x86_64-matrix\tools\depends\target\binary-addons\audiodecoder.sacd\lib\libdsdpcm\DSDPCMConverter.h(43): error C3779: '<lambda_389370868e5d87e756fda3a02ce50c72>::operator ()': a function that returns 'auto' cannot be used before it is defined
    d:\jenkins\workspace\binary-addons\kodi-windows-x86_64-matrix\tools\depends\target\binary-addons\audiodecoder.sacd\lib\libdsdpcm\DSDPCMConstants.h(28): note: see declaration of '<lambda_389370868e5d87e756fda3a02ce50c72>::operator ()'
    d:\jenkins\workspace\binary-addons\kodi-windows-x86_64-matrix\tools\depends\target\binary-addons\audiodecoder.sacd\lib\libdsdpcm\DSDPCMConverter.h(44): error C3779: '<lambda_389370868e5d87e756fda3a02ce50c72>::operator ()': a function that returns 'auto' cannot be used before it is defined
    d:\jenkins\workspace\binary-addons\kodi-windows-x86_64-matrix\tools\depends\target\binary-addons\audiodecoder.sacd\lib\libdsdpcm\DSDPCMConstants.h(28): note: see declaration of '<lambda_389370868e5d87e756fda3a02ce50c72>::operator ()'
    NMAKE : fatal error U1077: 'C:\PROGRA~2\MIB055~1\2017\COMMUN~1\VC\Tools\MSVC\1416~1.270\bin\Hostx64\x64\cl.exe' : return code '0x2'
    ```

--- lib/libdsdpcm/DSDPCMConverter.h
+++ lib/libdsdpcm/DSDPCMConverter.h
@@ -39,11 +39,6 @@ enum class conv_type_e {
 template<typename real_t>
 class DSDPCMConverter {
 protected:
-	real_t  dsd_fir1_8_ctables[CTABLES(DSDFIR1_8_LENGTH)][256];
-	real_t  dsd_fir1_16_ctables[CTABLES(DSDFIR1_16_LENGTH)][256];
-	real_t  dsd_fir1_64_ctables[CTABLES(DSDFIR1_64_LENGTH)][256];
-	real_t  pcm_fir2_2_coefs[PCMFIR2_2_LENGTH];
-	real_t  pcm_fir3_2_coefs[PCMFIR3_2_LENGTH];
 	int     framerate;
 	int     dsd_samplerate;
 	int     pcm_samplerate;
